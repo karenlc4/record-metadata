0ca02e21529cffd3f445379e05fc51c8384e708a
Talon Hintze <114630220+hintzeto@users.noreply.github.com>
2024-11-20 14:38:24 -0700

    updating for docker lvp

--- Streamlit/heatmap test.py
+++
@@ -1,9 +0,0 @@
-#%%
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-flights = sns.load_dataset("flights")
-
-flights_matrix = flights.pivot_table(index="month", columns="year", values="passengers")
-# %%
